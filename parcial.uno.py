377a65cdbdf79e647e883d740ab8429474adcead
unknown <matuk79@gmail.com>
2023-05-26 14:59:05 -0300

    paso 2

--- parcial.uno.py
+++ parcial.uno.py
@@ -1,4 +1,5 @@
 import json
+import re
 
 def leer_archivo(archivo_json:str) -> list:
     lista_nba = []
@@ -31,7 +32,19 @@ def muestra_estadistica_por_indice(indice:int,lista:list) -> bool:
         print(mensaje)
         return flag
 
-muestra_estadistica_por_indice(1,lista_nba)
+#muestra_estadistica_por_indice(1,lista_nba)
+
+def guardar_csv(ruta:str,texto:str):
+    flag_resultado = False
+    with open(ruta,"w+") as archivo:
+        byte = archivo.write(texto) 
+        if byte != 0:
+            flag_resultado = True
+    if flag_resultado :
+        print("Se creó el archivo: {0}".format(ruta))
+    else:
+        print("Error al crear el archivo: {0}".format(ruta))
+    return flag_resultado
 
 def crear_texto_por_indice(indice:int,lista:list,flag:bool):
     if flag:
@@ -39,19 +52,81 @@ def crear_texto_por_indice(indice:int,lista:list,flag:bool):
         claves = list(lista[indice].keys()) 
         claves.remove("estadisticas")
         claves.remove("logros")
-        print(claves)
         clave_estadistica = list(lista[indice]["estadisticas"].keys())
         texto_valores = ""
+        for i in range(len(claves)):
+            claves[i] = claves[i].capitalize()
         for clave,valor in lista[indice].items():
             if clave in claves:
                 lista_valores.append(valor)
-        
+        for clave,valor in lista[indice]["estadisticas"].items():
+            if clave in clave_estadistica:
+                lista_valores.append(str(valor))
+        texto_valores = ",".join(lista_valores)
         for clave in clave_estadistica:
-            claves.append(clave.replace("_"," "))
+            claves.append(clave.replace("_"," ").capitalize())
         texto_claves = ",".join(claves)
-        print(texto_claves)
-        return texto_claves + '\n' + texto_valores
+        texto_csv = texto_claves + '\n' + texto_valores
+        ruta = "{0}.csv".format(lista[indice]["nombre"])
+        flag = guardar_csv(ruta,texto_csv)
+    else:
+        print("No usaste punto 2")
     
-texto = crear_texto_por_indice(0,lista_nba,True)
-print(texto)
+#crear_texto_por_indice(0,lista_nba,True)
+
+
+def muestra_logros_por_nombre(nombre:str,lista:list):
+    patron = rf"{nombre}"
+    mensaje = ""
+    for jugador in lista:
+        if re.search(patron,jugador["nombre"].lower()):
+            mensaje = "{0}:\n".format(jugador["nombre"])
+            for logro in jugador["logros"]:
+                mensaje += "{0}\n".format(logro)
+            print(mensaje)
+#muestra_logros_por_nombre("john",lista_nba)
+
+# def quick_sort_estadistica(lista:list,clave:str,flag:bool):
+#     lista_de = []
+#     lista_iz = []
+#     if len(lista) <= 1:
+#             return lista
+#     else:
+#         cantidad = len(lista)
+#         pivot = lista[0]
+#         for i in range(1,cantidad):
+#             if flag == True and lista[i]["estadisticas"][clave] > pivot["estadisticas"][clave] or flag == False and lista[i]["estadisticas"][clave] < pivot["estadisticas"][clave]:
+#                 lista_de.append(lista[i])
+#             elif flag == True and lista[i]["estadisticas"][clave] <= pivot["estadisticas"][clave] or flag == False and lista[i]["estadisticas"][clave] > pivot["estadisticas"][clave]:
+#                 lista_iz.append(lista[i])
+#     lista_iz = quick_sort_estadistica(lista_iz,clave,flag)
+#     lista_iz.append(pivot)
+#     lista_de = quick_sort_estadistica(lista_de,clave,flag)
+#     lista_iz.extend(lista_de)
+#     return lista_iz
+
+def quick_sort_(lista:list,clave:str,flag:bool):
+    lista_de = []
+    lista_iz = []
+    if len(lista) <= 1:
+            return lista
+    else:
+        cantidad = len(lista)
+        pivot = lista[0]
+        for i in range(1,cantidad):
+            if flag == True and lista[i][clave] > pivot[clave] or flag == False and lista[i][clave] < pivot[clave]:
+                lista_de.append(lista[i])
+            elif flag == True and lista[i][clave] <= pivot[clave] or flag == False and lista[i][clave] > pivot[clave]:
+                lista_iz.append(lista[i])
+    lista_iz = quick_sort_(lista_iz,clave,flag)
+    lista_iz.append(pivot)
+    lista_de = quick_sort_(lista_de,clave,flag)
+    lista_iz.extend(lista_de)
+    return lista_iz
+
+lista = quick_sort_(lista_nba,"nombre",True)
+mensaje = ""
+for jugador in lista:
+    mensaje += "Nombre: {0} - puntos por partido: {1}\n".format(jugador["nombre"],jugador["estadisticas"]["promedio_puntos_por_partido"])
+print(mensaje)
 
